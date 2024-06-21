27c0c95d6d8faca9c34ba81e69192583244f2b88
Booker <bookerngoon@gmail.com>
2024-06-21 09:48:20 -0400

    daily algo

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -1,12 +1,11 @@
-const twoSum = function(nums, target) {
-    const hash = {}; // Stores seen numbers: {seenNumber: indexItOccurred}
-  
-    for (let i = 0; i < nums.length; i++) { // loop through all numbers
-      const n = nums[i]; // grab the current number `n`.
-      if (hash[target - n] !== undefined) { // check if the number we need to add to `n` to reach our target has been seen:
-        return [hash[target - n], i]; // grab the index of the seen number, and the index of the current number
+function twoSum(nums, target) {
+  const hash = {}; // Create an object to store numbers and their indices
+  for (let i = 0; i < nums.length; i++) {
+      const numberNeeded = target - nums[i]; // Calculate the complement
+      if (numberNeeded in hash) { // Check if the complement exists in the hash
+          return [hash[numberNeeded], i]; // Return the indices of the two numbers
       }
-      hash[n] = i; // update our hash to include the. number we just saw along with its index.
-    }
-    return []; // If no numbers add up to equal the `target`, we can return an empty array
+      hash[nums[i]] = i; // Store the current number and its index in the hash
   }
+};
+
