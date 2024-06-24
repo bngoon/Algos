07d63ee277cc5d54f21c9b6509c62374f9f5a64a
Booker <bookerngoon@gmail.com>
2024-06-24 10:13:57 -0400

    daily algo

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -9,3 +9,15 @@ function twoSum(nums, target) {
   }
 };
 
+
+
+function twoSum(nums, target){
+  for(let i = 0; i < nums.length; i++){
+    for(let j = 1; j <nums.length; j++){
+      if(nums[i] + nums[j] === target){
+        return [i, j]
+      }
+    }
+  }
+
+};
