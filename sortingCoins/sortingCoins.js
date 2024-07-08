a4db85d2f8613655c2fbe1e863e9b83116ec7d3e
Booker <bookerngoon@gmail.com>
2024-07-08 10:53:00 -0400

    daily algo

--- sortingCoins/sortingCoins.js
+++ sortingCoins/sortingCoins.js
@@ -1,13 +1,13 @@
 function nonConstructibleChange(coins){
-    let currentChangeCreated = 0;
-    let sortedCoins = coins.sort((a,b) (a - b));
+    let currentChangeCreated = 0; // initialize to 0
+    let sortedCoins = coins.sort((a, b) => a - b); // sort the coins array
 
-    for (let i = 0; i < sortedCoins.length; i++) {
-        let coin = sortedCoins[i];
-        if(coin > currentChangeCreated + 1){
+    for (let i = 0; i < sortedCoins.length; i++) {//iterate through the sorted coins array
+        let coin = sortedCoins[i];//set variable to index of sortedCoins array
+        if(coin > currentChangeCreated + 1){// check if the value of coin is greater than currentchagne + 1
             break;
         }
-        currentChangeCreated += coin
+        currentChangeCreated += coin//add values together 
     }
     return currentChangeCreated + 1;
 }
