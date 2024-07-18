d3aa211f8de9de8e96d4d696c147f8a27a6b38a5
Booker <bookerngoon@gmail.com>
2024-07-18 10:45:04 -0400

    daily algo

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -4,11 +4,12 @@ function commonCharacters(words) {
     // Loop through each character in the first word
     for (const letter of words[0]) {
       // Check if the letter is present in every word
+      // If so, add it to the result array
       if (words.every(word => word.includes(letter))) {
-        // If so, add it to the result array
-        resultArray.push(letter);
+        if(!resultArray.includes(letter))
+            resultArray.push(letter);
         // Replace the first occurrence of the letter in each word to avoid duplicate counting
-        words = words.map(word => word.replace(letter, ''));
+            words = words.map(word => word.replace(letter, ''));
       }
     }
   
