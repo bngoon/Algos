abf6bad6b1143a3c68e8cecd82e970188886e996
Booker <bookerngoon@gmail.com>
2024-07-19 10:21:09 -0400

    algos

---
+++ semordnilap/semordnilap.js
@@ -0,0 +1,19 @@
+function semordnilap(words) {
+    let resultArray = [];
+    const set = new Set();
+    
+    for (let char of words) {
+      const reversed = char.split('').reverse().join('');
+      if (set.has(reversed) && reversed !== char) {
+        resultArray.push([char, reversed])
+      }
+      set.add(char)
+    }
+  
+  
+    return resultArray;
+  }
+  
+  console.log(semordnilap(['diaper', 'repaid', 'kayak', 'lipstick', 'stressed', 'desserts']));
+  
+  
