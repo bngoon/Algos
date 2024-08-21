d0cc31a98bdf083eaa1c946bc1c1fbe3f8f10bb2
Booker <bookerngoon@gmail.com>
2024-08-21 09:19:31 -0400

    aglo

--- binarySearch/index.js
+++ binarySearch/index.js
@@ -13,3 +13,21 @@ function binarySearch(arr, target){
     }
     return - 1
 }
+
+
+function vanillaBinarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while(left <= right){
+        const mid = left + Math.floor((right - left) / 2);
+        if(arr[mid] === target) return mid;
+        if(arr[mid] < target){
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    
+    return -1;
+}
