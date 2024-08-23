fa457a2edc64437d14f4eb1b4bef13cd45329b28
Booker <bookerngoon@gmail.com>
2024-08-23 10:18:38 -0400

    algos

--- transposeMatrix/transposeMatrix.js
+++ transposeMatrix/transposeMatrix.js
@@ -1,21 +1,17 @@
 function transpose(matrix) {
-    // Initialize the new matrix with the same number of columns as the original matrix has rows
     let newMatrix = [];
-    
-    // Loop to create the necessary structure of the new matrix (transpose)
-    for(let i = 0; i < matrix[0].length; i++) {
-        newMatrix.push([]); // Each new row is an empty array
+
+    for(let i = 0; i < matrix[0].length; i++){
+        newMatrix.push([])
     }
-    
-    // Loop through each element of the original matrix
-    for(let y = 0; y < matrix.length; y++) {
-        for(let x = 0; x < matrix[0].length; x++) {
-            newMatrix[x][y] = matrix[y][x]; // Swap rows and columns
+
+    for(let row = 0; row < matrix[0].length; row++){
+        for(let column = 0; column < matrix.length; column++){
+            newMatrix[row][column] = matrix[column][row]
         }
     }
-    
-    return newMatrix; // Return the transposed matrix
-}
+    return newMatrix
+};
 
 // Example usage:
 let matrix = [
