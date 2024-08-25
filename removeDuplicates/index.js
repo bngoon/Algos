71ea15899fdf1f2de4146ad1bf9f3ba629aa890d
Booker <bookerngoon@gmail.com>
2024-08-25 18:18:28 -0400

    algo

---
+++ removeDuplicates/index.js
@@ -0,0 +1,12 @@
+function removedDuplicates(nums){
+    if(nums.length === 0) return 0
+    let slow = 0;
+
+    for(let fast = 1; fast < nums.length; fast++){
+        if(nums[fast] !== nums[slow]){
+            slow++
+            nums[slow] = nums[fast]
+        }
+    }
+    return slow + 1
+}
