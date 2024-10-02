df300ddce46c0887eb25fd3b53dc50857b6db1af
Booker <bookerngoon@gmail.com>
2024-10-02 09:29:08 -0400

    algos

--- DFS/MaxDepthOfTree/index.js
+++ DFS/MaxDepthOfTree/index.js
@@ -1,15 +1,15 @@
 class Node {
-    constructor(val, left = null, right = null) {
+    constructor(val, left = null, right = null) { // constructor with default values
         this.val = val;
         this.left = left;
         this.right = right;
     }
 }
-function dfs(root) {
-    if (root === null) return 0;
-    return Math.max(dfs(root.left), dfs(root.right)) + 1;
+function dfs(root) { // recursive depth first search / helper function
+    if (root === null) return 0; // base case
+    return Math.max(dfs(root.left), dfs(root.right)) + 1; // recursive case - return max depth of left and right subtrees + 1
 
 }
-function treeMaxDepth(root) {
-    return (root !== null) ? dfs(root) - 1 : 0;
+function treeMaxDepth(root) { // main function 
+    return (root !== null) ? dfs(root) - 1 : 0; // return -1 if root is null or else return dfs(root) - 1
 }
