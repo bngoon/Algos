7c13132867e007b92f6239223e8e78eb08a424d8
Booker <bookerngoon@gmail.com>
2024-10-04 11:33:33 -0400

    algos

--- peakIdxMountainArray/index.js
+++ peakIdxMountainArray/index.js
@@ -1,17 +1,17 @@
 function peakOfMountainArray(arr) {
-    let left = 0;
-    let right = arr.length - 1
-    let idx = -1
-    
-    while(left <= right){
-        let mid = left + Math.floor((right - left) / 2)
-        if(arr[mid] > arr[mid + 1]){
-           idx = mid
-           right = mid - 1
-    } else {
-           left = mid + 1 
-       }
-    }    
-    
-    return idx  ;
+    let left = 0; // index of the first element in the array
+    let right = arr.length - 1 // index of the last element in the array
+    let idx = -1 // index of the peak element
+
+    while (left <= right) { // while the left index is less than or equal to the right index
+        let mid = left + Math.floor((right - left) / 2) // calculate the middle index
+        if (arr[mid] > arr[mid + 1]) { // if the element at the middle index is greater than the element at the next index
+            idx = mid // set the index of the peak element to the middle index
+            right = mid - 1 // set the right index to the middle index minus 1
+        } else {
+            left = mid + 1 // set the left index to the middle index plus 1
+        }
+    }
+
+    return idx; // return the index of the peak element
 }
