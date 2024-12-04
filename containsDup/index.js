ebbaa21b2c4d00c12ba10753b399f73b5c2f5165
Booker <bookerngoon@gmail.com>
2024-12-04 12:12:38 -0500

    daily algo

--- containsDup/index.js
+++ containsDup/index.js
@@ -3,3 +3,13 @@ function containsDup(nums){
 
     return set.size !== nums.length
 }
+
+function containsDup2(nums){
+    let numSet = new Set();
+
+    for(let num of nums){
+        if(numSet.has(num)) return true;
+        numSet.add(num)
+    } 
+    return false;
+}
